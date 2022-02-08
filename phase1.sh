8533e3b342fca4044faedc0ccf25d7f22914abe2
Rui Gouveia <rgouveia@redhat.com>
2022-02-08 01:00:34 +0000

    second batch

--- phase1.sh
+++ phase1.sh
@@ -19,8 +19,10 @@ LIBVIRT_URI=$LIBVIRT_URI
 LIBVIRT_STORAGE=$LIBVIRT_STORAGE
 LIBVIRT_NETWORK_PREFIX=$LIBVIRT_NETWORK_PREFIX
 
-DOMAIN=$DOMAIN
+CLUSTER_DOMAIN=$CLUSTER_DOMAIN
 CLUSTER_NAME=$CLUSTER_NAME
+CLUSTER_VERSION=$CLUSTER_VERSION
+DISCONNECTED=$DISCONNECTED
 
 Do you want to continue? Press Enter or CTRL+C to abort."
 
@@ -80,5 +82,13 @@ sudo virsh connect qemu:///system
 
 
 # Create network for cluster.
-netxml=$(eval "echo \"$(cat files.phase1/network.xml)\"")
-echo $netxml
+if [ $DISCONNECTED ] ; then
+    netxml=$(eval "echo \"$(cat files.phase1/virt-network-isolated.xml)\"")
+else
+    netxml=$(eval "echo \"$(cat files.phase1/virt-network-nat.xml)\"")
+fi
+echo $netxml > /tmp/network-tmp.xml
+
+sudo virsh net-define --file /tmp/network-tmp.xml
+sudo virsh net-autostart $CLUSTER_NAME
+sudo virsh net-start $CLUSTER_NAME
