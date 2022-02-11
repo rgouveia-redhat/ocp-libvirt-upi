d8fc9dee3469db2a004ca074ca3caa2addfb13f0
Rui Gouveia <rgouveia@redhat.com>
2022-02-11 15:58:43 +0000

    Code to create libvirt storage pool

--- phase1.sh
+++ phase1.sh
@@ -16,9 +16,9 @@ Feel free to adjust the values to your needs.
 These are the current values for your reference:
 
 LIBVIRT_URI=$LIBVIRT_URI
-LIBVIRT_STORAGE=$LIBVIRT_STORAGE
+#LIBVIRT_STORAGE=$LIBVIRT_STORAGE
 LIBVIRT_NETWORK_PREFIX=$LIBVIRT_NETWORK_PREFIX
-LIBVIRT_POOL_NAME=$LIBVIRT_POOL_NAME
+LIBVIRT_STORAGE_POOL_BASE=$LIBVIRT_STORAGE_POOL_BASE
 
 CLUSTER_DOMAIN=$CLUSTER_DOMAIN
 CLUSTER_NAME=$CLUSTER_NAME
@@ -83,21 +83,30 @@ sudo virsh connect qemu:///system
 
 
 # Create network for cluster.
-net_exists=$(sudo virsh net-list --all --name | grep $CLUSTER_NAME)
-if [ "$net_exists" != "" ] ; then
+if [ "$(sudo virsh net-list --all --name | grep $CLUSTER_NAME)" != "" ] ; then
     echo "INFO: Network already exists."
 else
     if [ $DISCONNECTED ] ; then
-        netxml=$(eval "echo \"$(cat files.phase1/virt-network-isolated.xml)\"")
+        xml=$(eval "echo \"$(cat files.phase1/virt-network-isolated.xml)\"")
     else
-        netxml=$(eval "echo \"$(cat files.phase1/virt-network-nat.xml)\"")
+        xml=$(eval "echo \"$(cat files.phase1/virt-network-nat.xml)\"")
     fi
-    echo $netxml > /tmp/network-tmp.xml
+    echo $xml > /tmp/network-tmp.xml
 
     sudo virsh net-define --file /tmp/network-tmp.xml
     sudo virsh net-autostart $CLUSTER_NAME
     sudo virsh net-start $CLUSTER_NAME
+    rm /tmp/network-tmp.xml
 fi
 
-
-
+# Create the storage pool for the cluster.
+if [ "$(sudo virsh pool-list --all --name | grep $CLUSTER_NAME)" != "" ] ; then
+    echo "INFO: Storage Pool already exists."
+else
+    mkdir -p $LIBVIRT_STORAGE_POOL_BASE/$CLUSTER_NAME
+    sudo virsh pool-create-as --print-xml --name $CLUSTER_NAME --type dir --target $LIBVIRT_STORAGE_POOL_BASE/$CLUSTER_NAME > /tmp/pool-tmp.xml
+    sudo virsh pool-define --file /tmp/pool-tmp.xml
+    sudo virsh pool-autostart $CLUSTER_NAME
+    sudo virsh pool-start $CLUSTER_NAME
+    rm /tmp/pool-tmp.xml
+fi
