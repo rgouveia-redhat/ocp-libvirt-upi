bcbe44d38862997b4f62c434ed391ec50a8c6940
Rui Gouveia <rgouveia@redhat.com>
2022-03-12 01:46:51 +0000

    Ansible tasks re-organized

--- script/configure.sh
+++ script/configure.sh
@@ -63,38 +63,4 @@ configure_bastion () {
 
     # Back to script folder.
     cd ..
-
-    exit
-
-
-    echo
-    echo "$(date +%T) INFO: Phase 2 - Preparing Bastion VM for the OpenShift Install."
-
-    # Go to Ansible folder.
-    cd ansible
-
-    echo "$(date +%T) INFO: Check to make sure Ansible can proceed."
-    ansible \
-        --private-key=../ssh/id_rsa \
-        --ssh-extra-args="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
-        -u root -i hosts all -m ping
-
-    if [ $? -eq 0 ]; then
-        echo "$(date +%T) INFO: Running Ansible Playbook to configure Bastion..."
-
-        ansible-playbook \
-            --private-key=../ssh/id_rsa \
-            --ssh-extra-args="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
-            -u root -i hosts bastion-phase2.yaml
-        if ! [ $? -eq 0 ]; then
-            echo "$(date +%T) ERROR: Ansible exited with errors. Fix the errors and rerun ./phase2.sh !"
-            exit 10
-        fi
-    else
-        echo "$(date +%T) ERROR: Ansible test failed. Fix the issue and run this command again."
-        exit 100
-    fi
-
-    # Back to script folder.
-    cd ..
 }
