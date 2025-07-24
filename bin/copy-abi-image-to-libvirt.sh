6fd4c22d11224aca850c6d75bcb430b3138670bc
Rui Gouveia <rgouveia@redhat.com>
2025-07-24 16:16:22 +0100

    Avoid bastion ssh unknown keys.

--- bin/copy-abi-image-to-libvirt.sh
+++ bin/copy-abi-image-to-libvirt.sh
@@ -3,7 +3,10 @@
 # TODO: Check if file exists.
 network_prefix=$(cat .re-run-with-network)
 
-sudo scp -i ./ssh/id_rsa root@${network_prefix}.3:/root/install-dir/agent.x86_64.iso /var/lib/libvirt/images/
+sudo scp -i ./ssh/id_rsa \
+    -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+    root@${network_prefix}.3:/root/install-dir/agent.x86_64.iso /var/lib/libvirt/images/
+
 sudo chown qemu:qemu /var/lib/libvirt/images/agent.x86_64.iso
 sudo restorecon -F /var/lib/libvirt/images/agent.x86_64.iso
 sudo ls -lZ /var/lib/libvirt/images/
